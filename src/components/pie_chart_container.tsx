3247786d14f3a798288ad050d29a2935033b68d6
Jon <jon.walz.music@gmail.com>
2019-05-12 22:06:17 -0700

    removed padding on chart

--- src/components/pie_chart_container.tsx
+++ src/components/pie_chart_container.tsx
@@ -10,7 +10,7 @@ export const ChartContainer = (): JSX.Element => {
     const transformedData = getLanguageData(payload)
 
     return (
-        <div style={{ width: '100%', height: '80vh', paddingLeft: "3em", paddingRight: "3em" }}>
+        <div style={{ width: '100%', height: '80vh' }}>
             <h3 style={{textAlign: "center"}}>Treehouse Points</h3>
             <PieChart data={transformedData} error={error} />
         </div>
